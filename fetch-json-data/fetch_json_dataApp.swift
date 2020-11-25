b0eac12e5cb5e0417b6b120eb438cb36a11fdc77
Rick Brown <ricky.brown.00@gmail.com>
2020-11-25 11:19:01 +0000

    Initial Commit

---
+++ fetch-json-data/fetch_json_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_json_dataApp.swift
+//  fetch-json-data
+//
+//  Created by Rick Brown on 25/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct fetch_json_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
